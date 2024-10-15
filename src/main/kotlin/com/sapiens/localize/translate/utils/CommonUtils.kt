3365274444db2ea299365c9909d39bf33a62a2cf
John <kai.wang.john@gmail.com>
2024-10-15 15:12:16 +0800

    update jar path util

--- src/main/kotlin/com/sapiens/localize/translate/utils/CommonUtils.kt
+++ src/main/kotlin/com/sapiens/localize/translate/utils/CommonUtils.kt
@@ -2,7 +2,6 @@ package com.sapiens.localize.translate.utils
 
 import com.google.gson.GsonBuilder
 import java.io.File
-import java.nio.file.Paths
 
 
 fun safeRun(printLog: Boolean = true, block: () -> Unit) {
@@ -23,13 +22,11 @@ fun gson() = GsonBuilder().disableHtmlEscaping().create()
 
 private fun jarPath(): String {
 /// 获取当前的 class path
-    val classPath = System.getProperty("java.class.path")
-    // 获取运行的 JAR 文件的路径
-    val jarPath = classPath.split(File.pathSeparator).firstOrNull { it.endsWith(".jar") }
-    // 如果找不到 JAR 文件路径，则抛出异常
-    if (jarPath == null) {
-        throw RuntimeException("Can't find jar path")
-    }
-    // 返回 JAR 文件的规范化目录
-    return File(jarPath).parentFile.canonicalPath
+    val jarFilePath = object {}.javaClass.protectionDomain.codeSource.location.toURI().path
+    val jarFile = File(jarFilePath)
+
+    // 获取父目录路径
+    val parentFile = jarFile.parentFile ?: throw RuntimeException("Can't find parent directory for jar file")
+
+    return parentFile.canonicalPath
 }
